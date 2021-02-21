04ff6e99c224ef7ba62665e67b1f2ee47c5e4e30
Lalo Luna <57363601+laloluna14@users.noreply.github.com>
2021-02-21 17:32:31 -0600

    hw_02

--- hw_02_github_bot.py
+++ hw_02_github_bot.py
@@ -4,3 +4,47 @@
 input('Users:')
 galileoguzman, frasgado, laloluna14, ....
 '''
+
+import requests
+
+# Importing functions from another file
+from github_user_function import get_user, download_file
+
+# Modified function
+def download_file(url_file, username):
+    '''
+    Name: download_file
+    Params: url_file -> String
+    Returns: None or image file
+    '''
+    response = requests.get(url_file)
+    if response.status_code != 200:
+        return None
+    response_content = response.content # Store response content to be save later
+    filename = f'tmp/{username}.png'
+    with open(filename, 'wb') as image:
+        image.write(response_content)
+        return image
+
+usernames = []
+users = []
+cycle = True
+
+# Getting the usernames
+while(cycle):
+    username = input('Give me an username: ')
+    usernames.append(username)
+
+    add = input('Do you want to add more users? (y/n): ')
+
+    if(add == 'n'):
+        cycle = False
+
+# Getting the records of the users
+for username in usernames:
+    users.append(get_user(username))
+
+# Getting and downloading the avatar image of the user
+for user in users:
+    url_file = user['avatar_url']
+    download_file(url_file, user['login'])
