468e9c0c80919f6f2e943aed7a4a1dac9f9579d3
Lalo Luna <57363601+laloluna14@users.noreply.github.com>
2021-02-21 17:17:40 -0600

    hw_01

--- hw_01_github_boy.py
+++ hw_01_github_boy.py
@@ -1,3 +1,18 @@
 '''
 Ask for a username and download his/her avatar image
 '''
+
+# Importing functions from another file
+from github_user_function import get_user, download_file
+
+# Getting the username
+username = input('Give me an username: ')
+
+# Getting the records of the user
+user = get_user(username)
+
+# Getting the avatar image of the user
+url_file = user['avatar_url']
+
+# Downloading the avatar image of the user
+download_file(url_file)
